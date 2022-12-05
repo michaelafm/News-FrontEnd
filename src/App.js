69fd4b420e5e13d8154282e1e032cebaa869c290
michaelafm <105201811+michaelafm@users.noreply.github.com>
2022-12-05 14:46:46 +0000

    ncnews-3-single-article created SingleArticle component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
-import Header from "./components/Header";
-import Articles from "./components/Articles";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Articles from "./components/Articles";
+import SingleArticle from "./components/SingleArticle";
+
 
 
 function App() {
@@ -11,10 +14,15 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Nav />
       <Routes> 
         <Route
             path="/"
             element={ <Articles articles={articles} setArticles={setArticles}/>}
+          />
+           <Route
+            path="/article/:article_id"
+            element={<SingleArticle setArticles={setArticles} />}
           />
       </Routes>
     </div>
---
+++ src/components/SingleArticle.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticleById } from "../utils/api";
+
+function SingleArticle({ setArticles }) {
+  const [singleArticle, setSingleArticle] = useState({});
+
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    getArticleById(article_id).then((retreivedArticle) => {
+      setSingleArticle(retreivedArticle);
+    });
+  }, [article_id]);
+
+  return (
+    <main>
+      <div className="SingleArticle">
+        <h3>{singleArticle.title}</h3>
+        <p>{singleArticle.body}</p>
+        <p>Written by: {singleArticle.author} </p>
+        <p>Topic: {singleArticle.topic}</p>
+        <p>Created: {new Date(singleArticle.created_at).toGMTString()}</p>
+        <p>Votes: {singleArticle.votes}</p>
+        <p>Comment count: {singleArticle.comment_count}</p>
+      </div>
+    </main>
+  );
+}
+
+export default SingleArticle;
