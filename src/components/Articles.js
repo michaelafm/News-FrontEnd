ba69d8f7404e57d08a1a3bf92d326ce110481f33
michaelafm <105201811+michaelafm@users.noreply.github.com>
2022-12-09 18:38:24 +0000

    fixed SortByQuery conditional rendering in Articles component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import { getArticles } from "../utils/api";
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import { Link, useParams, useSearchParams, useLocation} from "react-router-dom";
 import { Card } from "grommet";
 import SortByQuery from "./SortByQuery";
 import ErrorPage from "./ErrorPage";
-
+ 
 function Articles() {
   const [articles, setArticles] = useState([]);
   const [loadingArticles, setLoadingArticles] = useState(true);
   const { topic } = useParams();
   let [searchParams, setSearchParams] = useSearchParams();
   const [error, setError] = useState(null);
+  const [previousLocation, setPreviousLocation] = useState('')
+  const location = useLocation();
 
   let sort_by = searchParams.get("sort_by");
   let order = searchParams.get("order");
 
   useEffect(() => {
+    setPreviousLocation(location.pathname)
     setError(null);
     setLoadingArticles(true);
     getArticles(sort_by, order, topic)
@@ -26,7 +29,7 @@ function Articles() {
       .catch((err) => {
         setError({ err });
       });
-  }, [sort_by, order, topic]);
+  }, [sort_by, order, topic, location.pathname]);
 
   if (error) {
     return (
@@ -39,19 +42,17 @@ function Articles() {
 
   return (
     <main>
+      <div className="Articles_header_container">
+        {topic ? <h2>{topic} articles</h2> : <h2>All articles</h2>}
+        {location.pathname === previousLocation ?  (<SortByQuery
+          setSearchParams={setSearchParams}
+          searchParams={searchParams}
+        />): (null)}
+      </div>
       {loadingArticles ? (
         <p>...loading articles</p>
       ) : (
         <div className="Articles">
-          <div className="Articles_header_container">
-            {topic ? (
-              <h2 >{topic} articles</h2>
-            ) : <h2>All articles</h2>}
-            <SortByQuery
-              setSearchParams={setSearchParams}
-              searchParams={searchParams}
-            />
-          </div>
           <ul className="Articles_container">
             {articles.map((article) => {
               return (
