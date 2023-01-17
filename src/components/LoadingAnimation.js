8e366e88abb096262d896168ec57dfcfd36f1d6b
michaelafm <105201811+michaelafm@users.noreply.github.com>
2023-01-17 16:32:18 +0000

    removed react-lottie

--- src/components/LoadingAnimation.js
+++ src/components/LoadingAnimation.js
@@ -1,24 +1,23 @@
 import React from "react";
-import Lottie from 'react-lottie';
-import animationData from '../lotties/98635-loading.json';
+// import animationData from '../lotties/98635-loading.json';
 
 const LoadingAnimation = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: "xMidYMid slice"
+  //   }
+  // };
 
   return (
     <div>
-      <Lottie
+      {/* <Lottie
         options={defaultOptions}
         height={400}
         width={400}
-        />
+        /> */}
     </div>
   );
 };
